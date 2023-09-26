af7b150322bbd8bd907e34f7ab05ccadda92452b
NguyenHuyHoang <145429096+HoangCuns@users.noreply.github.com>
2023-09-26 21:56:51 +0700

    Add files via upload

---
+++ list.py
@@ -0,0 +1,12 @@
+L = [1, 2, 3, 4, 5, 6]
+print(type(L)) # In ra kiểu dữ liệu của L, kết quả là '<class 'list's
+
+# Thêm một phần tử 100 vào cuối danh sách
+L.append(100)
+
+# Truy cập và in ra các phần tử trong danh sách
+
+print("The first item is", L[0])
+print("The second item is", L[1])
+print("The last item is", L[-1])
+print("The second to last item is", L[-2])
---
+++ string.py
@@ -0,0 +1,8 @@
+S = "Hello,
+t = "World."
+u = s + t
+print(type(u)) # In ra kiểu dữ liệu của biến u
+print(u) # In ra giá trị của biển u
+print(u[9]) # Truy cập ký tự thứ 9 trong chuỗi u
+n = str(9876)
+print(n[2]) # Truy cập ký tự thứ 2 trong chuỗi n
