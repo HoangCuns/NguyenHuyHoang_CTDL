1500d9cba5f23a8ec9fedd17af3ebd2d32d66073
NguyenHuyHoang <145429096+HoangCuns@users.noreply.github.com>
2023-09-26 20:50:20 +0700

    Add files via upload

---
+++ vidu.py
@@ -0,0 +1,11 @@
+# Bước 1: Nhập số thứ nhất từ người dùng
+num1 = float(input("Nhập số thứ nhất: "))
+
+# Bước 2: Nhập số thứ hai từ người dùng
+num2 = float(input("Nhập số thứ hai: "))
+
+# Bước 3: Tính tổng của hai số tong = num1 + num2
+tong = num1 + num2
+
+# Bước 4: Hiển thị kết quả
+print("Tổng của", num1, "và", num2, "là", tong)
